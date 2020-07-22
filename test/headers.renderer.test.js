bd480e952fc098117d8ec564cd41b3c7de1256f9
Pawel <jarrodek@gmail.com>
2020-07-21 23:24:52 -0700

    chore: adding types

--- test/headers.renderer.test.js
+++ test/headers.renderer.test.js
@@ -1,18 +1,18 @@
 const { assert } = require('chai');
 const { ArcHeaders } = require('../');
 
-describe('ArcHeaders tests', function() {
+describe('ArcHeaders tests', () => {
   [
     ['String', 'x-a:a\nx-b:b\nc:c', 3, ['x-a', 'x-b', 'c'], ['a', 'b', 'c']],
     ['Map', {
       'x-a': 'a',
       'x-b': 'b',
-      'c': 'c'
+      'c': 'c',
     }, 3, ['x-a', 'x-b', 'c'], ['a', 'b', 'c']],
     ['Array', [['x-a', 'a'], ['x-b', 'b'], ['c', 'c']], 3, ['x-a', 'x-b', 'c'], ['a', 'b', 'c']],
-    ['undefined', undefined, 0, [], []]
+    ['undefined', undefined, 0, [], []],
   ].forEach((item) => {
-    describe(`${item[0]} input`, function() {
+    describe(`${item[0]} input`, () => {
       it(`Parses ${item[0]} as an input`, () => {
         const instance = new ArcHeaders(item[1]);
         assert.lengthOf(Object.keys(instance.map), item[2]);
@@ -31,115 +31,115 @@ describe('ArcHeaders tests', function() {
     });
   });
 
-  describe('append()', function() {
-    it('Appends an item that doesnt exists', function() {
+  describe('append()', () => {
+    it('Appends an item that doesnt exists', () => {
       const instance = new ArcHeaders();
       instance.append('name', 'value');
       assert.equal(instance.map.name.value, 'value');
       assert.equal(instance.map.name.name, 'name');
     });
 
-    it('Updates existing item', function() {
+    it('Updates existing item', () => {
       const instance = new ArcHeaders();
       instance.append('name', 'value');
       instance.append('name', 'other-value');
       assert.equal(instance.map.name.value, 'value,other-value');
     });
 
-    it('Normalizes keys', function() {
+    it('Normalizes keys', () => {
       const instance = new ArcHeaders();
       instance.append('AbCd', 'value');
       assert.ok(instance.map.abcd);
     });
   });
 
-  describe('set()', function() {
-    it('Sets an item that doesnt exists', function() {
+  describe('set()', () => {
+    it('Sets an item that doesnt exists', () => {
       const instance = new ArcHeaders();
       instance.set('name', 'value');
       assert.equal(instance.map.name.value, 'value');
       assert.equal(instance.map.name.name, 'name');
     });
 
-    it('Replaces existing item', function() {
+    it('Replaces existing item', () => {
       const instance = new ArcHeaders();
       instance.set('name', 'value');
       instance.set('name', 'other-value');
       assert.equal(instance.map.name.value, 'other-value');
     });
 
-    it('Normalizes keys', function() {
+    it('Normalizes keys', () => {
       const instance = new ArcHeaders();
       instance.set('AbCd', 'value');
       assert.ok(instance.map.abcd);
     });
   });
 
-  describe('delete()', function() {
-    it('Ignores item that doesnt exists', function() {
+  describe('delete()', () => {
+    it('Ignores item that doesnt exists', () => {
       const instance = new ArcHeaders('a: b');
       instance.delete('x');
       assert.ok(instance.map.a);
     });
 
-    it('Removes existing item', function() {
+    it('Removes existing item', () => {
       const instance = new ArcHeaders('a: b');
       instance.delete('a');
       assert.lengthOf(Object.keys(instance.map), 0);
     });
 
-    it('Normalizes keys', function() {
+    it('Normalizes keys', () => {
       const instance = new ArcHeaders('AbCd: xxx');
       instance.delete('aBcD');
       assert.lengthOf(Object.keys(instance.map), 0);
     });
   });
 
-  describe('get()', function() {
-    it('Returns undefined for non existing item', function() {
+  describe('get()', () => {
+    it('Returns undefined for non existing item', () => {
       const instance = new ArcHeaders('a: b');
       const result = instance.get('x');
       assert.isUndefined(result);
     });
 
-    it('Returns value for existing item', function() {
+    it('Returns value for existing item', () => {
       const instance = new ArcHeaders('a: b');
       const result = instance.get('a');
       assert.equal(result, 'b');
     });
 
-    it('Normalizes keys', function() {
+    it('Normalizes keys', () => {
       const instance = new ArcHeaders('AbCd: xxx');
       const result = instance.get('aBcD');
       assert.equal(result, 'xxx');
     });
   });
 
-  describe('has()', function() {
-    it('Returns false for non existing item', function() {
+  describe('has()', () => {
+    it('Returns false for non existing item', () => {
       const instance = new ArcHeaders('a: b');
       const result = instance.has('x');
       assert.isFalse(result);
     });
 
-    it('Returns value for existing item', function() {
+    it('Returns value for existing item', () => {
       const instance = new ArcHeaders('a: b');
       const result = instance.has('a');
       assert.isTrue(result);
     });
 
-    it('Normalizes keys', function() {
+    it('Normalizes keys', () => {
       const instance = new ArcHeaders('AbCd: xxx');
       const result = instance.has('aBcD');
       assert.isTrue(result);
     });
   });
 
-  describe('forEach()', function() {
+  describe('forEach()', () => {
     const input = 'AbCd: Test\nMy_Test: header';
     const keys = ['AbCd', 'My_Test'];
     const values = ['Test', 'header'];
-    it('Iterates over items', function() {
+    it('Iterates over items', () => {
       const instance = new ArcHeaders(input);
       instance.forEach((value, name) => {
         assert.notEqual(keys.indexOf(name), -1, `${name} is a name`);
@@ -148,23 +148,23 @@ describe('ArcHeaders tests', function() {
     });
   });
 
-  describe('toString()', function() {
+  describe('toString()', () => {
     const input = 'My_Test: header\nAbCd: Test';
-    it('Returns HTTP string', function() {
+    it('Returns HTTP string', () => {
       const instance = new ArcHeaders(input);
       const result = instance.toString();
       assert.equal(result, input);
     });
 
-    it('Returns HTTP string with multiple values', function() {
+    it('Returns HTTP string with multiple values', () => {
       const instance = new ArcHeaders(input);
       instance.append('abcd', 'test');
       const result = instance.toString();
-      assert.equal(result, input + ',test');
+      assert.equal(result, `${input },test`);
     });
   });
 
-  describe('keys()', function() {
+  describe('keys()', () => {
     const input = 'My_Test: header\nAbCd: Test';
     it('Is a generator', () => {
       const instance = new ArcHeaders(input);
@@ -174,7 +174,7 @@ describe('ArcHeaders tests', function() {
       assert.isFalse(next.done);
     });
 
-    it('Can iterate pover the keys', function() {
+    it('Can iterate pover the keys', () => {
       const instance = new ArcHeaders(input);
       const allowed = ['My_Test', 'AbCd'];
       for (const key of instance.keys()) {
@@ -183,7 +183,7 @@ describe('ArcHeaders tests', function() {
     });
   });
 
-  describe('values()', function() {
+  describe('values()', () => {
     const input = 'My_Test: header\nAbCd: Test';
     it('Is a generator', () => {
       const instance = new ArcHeaders(input);
@@ -193,7 +193,7 @@ describe('ArcHeaders tests', function() {
       assert.isFalse(next.done);
     });
 
-    it('Can iterate pover the values', function() {
+    it('Can iterate pover the values', () => {
       const instance = new ArcHeaders(input);
       const allowed = ['header', 'Test'];
       for (const key of instance.values()) {
@@ -202,7 +202,7 @@ describe('ArcHeaders tests', function() {
     });
   });
 
-  describe('entries()', function() {
+  describe('entries()', () => {
     const input = 'My_Test: header\nAbCd: Test';
     it('Is a generator', () => {
       const instance = new ArcHeaders(input);
@@ -212,7 +212,7 @@ describe('ArcHeaders tests', function() {
       assert.isFalse(next.done);
     });
 
-    it('Can iterate pover the entries', function() {
+    it('Can iterate pover the entries', () => {
       const instance = new ArcHeaders(input);
       const allowedKeys = ['My_Test', 'AbCd'];
       const allowedValues = ['header', 'Test'];
@@ -223,10 +223,10 @@ describe('ArcHeaders tests', function() {
     });
   });
 
-  describe('Symbol.iterator', function() {
+  describe('Symbol.iterator', () => {
     const input = 'My_Test: header\nAbCd: Test';
 
-    it('Can iterate pover the object', function() {
+    it('Can iterate pover the object', () => {
       const instance = new ArcHeaders(input);
       const allowedKeys = ['My_Test', 'AbCd'];
       const allowedValues = ['header', 'Test'];
